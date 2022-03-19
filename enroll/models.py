52e4588e1da77420f2b5b0667ba821041ffcda46
unknown <shukumar-542>
2022-03-19 22:18:44 +0600

    create database model

---
+++ enroll/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Students(models.Model):
+      stid = models.IntegerField()
+      stname = models.CharField(max_length=70)
+      stemail =models.EmailField(max_length=70)
+      stpass =models.CharField(max_length=70)
